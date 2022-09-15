c77b388576636c9fe79610bdac4dcdc49f8d812c
Thiago Fernandes <tjfctigo@gmail.com>
2022-09-15 09:43:16 -0300

    aula 02: layour

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,19 @@
+import React, { Children } from 'react';
+import { ImageBackground, View } from 'react-native';
+import { styles } from './styles';
+import backgroundImg from "../../assets/background-galaxy.png"
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function Background({children}:Props) {
+  return (
+    <ImageBackground 
+      source={backgroundImg}
+      defaultSource={backgroundImg}
+      style={styles.container}>
+      {children}
+    </ImageBackground>
+  );
+}
